20fc10ce27a89a3e1ac636c5dca2f052701bdcb9
Jeffrey Fisher <jeff@slofish.io>
2020-03-07 00:43:11 -0800

    Fix GitHub pjax issues (#5)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- background.js
+++ background.js
@@ -2,16 +2,22 @@
 
 chrome.webRequest.onBeforeRequest.addListener(function (details) {
 	if (details.method === 'GET') {
+		const searchParams = new URLSearchParams(details.url.searchParams);
+		searchParams.set('q', 'is:open');
+		const newUrl = new URL(details.url + '?' + searchParams.toString());
+
 		return {
-			redirectUrl: details.url + '?q=is:open'
+			redirectUrl: newUrl.href
 		};
 	}
 }, {
 	urls: [
-		'https://github.com/*/*/issues'
+		'https://github.com/*/*/issues',
+		'https://github.com/*/*/issues?_pjax=%23js-repo-pjax-container',
 	],
 	types: [
-		'main_frame'
+		'main_frame',
+		'xmlhttprequest'
 	]
 }, [
 	'blocking'
